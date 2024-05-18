38280075cc461b8b61ba90f77fb9344248dfb109
SamaraLisboa <samara.bueno@sptech.school>
2024-05-18 17:03:00 -0300

    adicionado livros e models na sessão de todos os gêneros

---
+++ Biblio's/biblioteca/biblioteca.js
@@ -0,0 +1,10 @@
+// Modal do livro "Dezesseis Luas"
+function modalDezesseisLuas() {
+    let dezesseisLuas = document.getElementById("modal");
+    dezesseisLuas.style.display = "block";
+}
+
+function sairModal() {
+    let sairModal = document.getElementById("modal")
+    sairModal.style.display = "none"
+}
