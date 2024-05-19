31c8effdd7a9a58abeacc588ab213a074fe03217
SamaraLisboa <samara.bueno@sptech.school>
2024-05-18 23:51:17 -0300

    criando modal para a página todos os gêneros

--- Biblio's/biblioteca/biblioteca.js
+++ Biblio's/biblioteca/biblioteca.js
@@ -1,10 +1,109 @@
 // Modal do livro "Dezesseis Luas"
 function modalDezesseisLuas() {
-    let dezesseisLuas = document.getElementById("modal");
-    dezesseisLuas.style.display = "block";
+    let abrirModal = document.getElementById("popup-dezesseisLuas");
+    abrirModal.style.display = "block";
+}
+function sairModalDezesseisLuas() {
+    let sairModal = document.getElementById("popup-dezesseisLuas")
+    sairModal.style.display = "none"
 }
 
-function sairModal() {
-    let sairModal = document.getElementById("modal")
+// Modal do livro "Hush Hush"
+function modalSussuro() {
+    let abrirModal = document.getElementById("popup-sussuro");
+    abrirModal.style.display = "block";
+}
+function sairModalSussurro() {
+    let sairModal = document.getElementById("popup-sussuro")
+    sairModal.style.display = "none"
+}
+
+// Modal do livro "Heaven Official's Blessing"
+function modalHeaven() {
+    let abrirModal = document.getElementById("popup-heaven");
+    abrirModal.style.display = "block";
+}
+function sairModalHeaven() {
+    let sairModal = document.getElementById("popup-heaven")
+    sairModal.style.display = "none"
+}
+
+// Modal do livro "Crepúsculo"
+function modalCrepusculo() {
+    let abrirModal = document.getElementById("popup-crepusculo");
+    abrirModal.style.display = "block";
+}
+function sairModalCrepusculo() {
+    let sairModal = document.getElementById("popup-crepusculo")
+    sairModal.style.display = "none"
+}
+
+// Modal do livro "A his´toria sem fim"
+function modalHistoriaSemFim() {
+    let abrirModal = document.getElementById("popup-historiaSemFim");
+    abrirModal.style.display = "block";
+}
+function sairModalHistoriaSemFim() {
+    let sairModal = document.getElementById("popup-historiaSemFim")
+    sairModal.style.display = "none"
+}
+
+// Modal do livro "Mo Dao Zu Shi"
+function modalMoDaoZuShi() {
+    let abrirModal = document.getElementById("popup-moDaoZuShi");
+    abrirModal.style.display = "block";
+}
+function sairModalMoDaoZuShi() {
+    let sairModal = document.getElementById("popup-moDaoZuShi")
+    sairModal.style.display = "none"
+}
+
+// Modal do livro "O Pequeno Príncipe"
+function modalPequenoPrincipe() {
+    let abrirModal = document.getElementById("popup-pequenoPrincipe");
+    abrirModal.style.display = "block";
+}
+function sairModalPequenoPrincipe() {
+    let sairModal = document.getElementById("popup-pequenoPrincipe")
+    sairModal.style.display = "none"
+}
+
+// Modal do livro "A Bússula de Ouro"
+function modalBussulaDeOuro() {
+    let abrirModal = document.getElementById("popup-bussuladeOuro");
+    abrirModal.style.display = "block";
+}
+function sairModalBussulaDeOuro() {
+    let sairModal = document.getElementById("popup-bussuladeOuro")
+    sairModal.style.display = "none"
+}
+
+// Modal do livro "Uzumaki"
+function modalUzumaki() {
+    let abrirModal = document.getElementById("popup-uzumaki");
+    abrirModal.style.display = "block";
+}
+function sairModalUzumaki() {
+    let sairModal = document.getElementById("popup-uzumaki")
+    sairModal.style.display = "none"
+}
+
+// Modal do livro "Tomie"
+function modalPequenoPrincipe() {
+    let abrirModal = document.getElementById("popup-pequenoPrincipe");
+    abrirModal.style.display = "block";
+}
+function sairModalPequenoPrincipe() {
+    let sairModal = document.getElementById("popup-pequenoPrincipe")
+    sairModal.style.display = "none"
+}
+
+// Modal do livro "Coraline"
+function modalPequenoPrincipe() {
+    let abrirModal = document.getElementById("popup-pequenoPrincipe");
+    abrirModal.style.display = "block";
+}
+function sairModalPequenoPrincipe() {
+    let sairModal = document.getElementById("popup-pequenoPrincipe")
     sairModal.style.display = "none"
 }
